c811513915a3dfeb4ad599552614ae8acaf2c9ad
Quan Nguyen <qmnguyen@iu.edu>
2021-09-14 11:57:38 -0400

    Small modifications to script

--- extract_tc_features.sh
+++ extract_tc_features.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env python3
 
-if __name__ == '__main__':
-    print('hello')
+import numpy as np
+import xarray as xr
 
+if __name__ == '__main__':
+    ds = xr.open_dataset(
+        './data_theanh/2020/gdas1.fnl0p25.2020010100.f00.grib2',
+        engine='cfgrib',
+        backend_kwargs={'indexpath': ''},
+        filter_by_keys={'typeOfLevel': 'atmosphere'})
+    print(ds, ds.attrs)
