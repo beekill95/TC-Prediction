357c586409486e7c380b5edacf41839994131e43
Quan Nguyen <qmnguyen@iu.edu>
2021-10-07 20:28:33 -0400

    Script for working with gpu server

---
+++ workspace/jupyter-gpu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Activate conda environment with tensorflow-gpu
+conda activate tc_prediction
+
+# Then, forward port back to h2
+port=8888
+ssh -N -f -R $port:localhost:$port h2
+
+# Finally, start jupyter notebook at the specified port.
+jupyter notebook --no-browser --ip 0.0.0.0 --port $port
