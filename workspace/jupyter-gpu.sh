c6b2559f7ff2677a2bb1ba58608ca92051b83275
Quan Nguyen <qmnguyen@iu.edu>
2021-10-11 15:08:07 -0400

    Source .bashrc before doing anything in gpu node

--- workspace/jupyter-gpu.sh
+++ workspace/jupyter-gpu.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Source our .bashrc, should be done automatically, but somehow don't
+source $HOME/.bashrc
+
 # Activate conda environment with tensorflow-gpu
 conda activate tc_prediction
 
