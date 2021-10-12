08e16875c8ff5405c86b07dad1cec4a229fdd132
Quan Nguyen <qmnguyen@iu.edu>
2021-10-12 15:56:54 -0400

    Add parameter to make bash load .bashrc

--- workspace/jupyter-gpu.sh
+++ workspace/jupyter-gpu.sh
@@ -1,14 +1,11 @@
-#!/bin/bash
-
-# Source our .bashrc, should be done automatically, but somehow don't
-source $HOME/.bashrc
+#!/bin/bash -i
 
 # Activate conda environment with tensorflow-gpu
 conda activate tc_prediction
 
 # Then, forward port back to h2
 port=8888
-ssh -N -f -R $port:localhost:$port h2
+ssh -N -f -R $port:localhost:$port h1
 
 # Finally, start jupyter notebook at the specified port.
 jupyter notebook --no-browser --ip 0.0.0.0 --port $port
