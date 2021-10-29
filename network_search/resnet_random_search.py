454aaa644929387d2ce4df4d308aba0793e9b365
Quan Nguyen <qmnguyen@iu.edu>
2021-10-29 13:56:41 -0400

    Network search with resnet18

---
+++ network_search/resnet_random_search.py
@@ -0,0 +1,130 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:light,ipynb
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+import sys  # noqa
+sys.path.append('..')  # noqa
+
+import models.resnet as resnet
+import keras_tuner as kt
+from tensorflow.keras.layers.experimental import preprocessing
+import tensorflow_addons as tfa
+import tf_metrics as tfm
+import data
+import tensorflow.keras as keras
+# -
+
+# # Randomize Search with Resnet
+
+
+# First, load training data and validation data.
+
+data_path = '/N/project/pfec_climo/qmnguyen/tc_prediction/extracted_features/alllevels_ABSV_CAPE_RH_TMP_HGT_VVEL_UGRD_VGRD/6h_700mb'
+train_path = f'{data_path}_train'
+val_path = f'{data_path}_val'
+test_path = f'{data_path}_test'
+data_shape = (41, 181, 135)
+
+full_training = data.load_data(
+    train_path,
+    data_shape=data_shape,
+    batch_size=64,
+    shuffle=True,
+)
+downsampled_training = data.load_data(
+    train_path,
+    data_shape=data_shape,
+    batch_size=64,
+    shuffle=True,
+    negative_samples_ratio=1)
+validation = data.load_data(val_path, data_shape=data_shape)
+
+normalizer = preprocessing.Normalization(axis=-1)
+for X, y in iter(full_training):
+    normalizer.adapt(X)
+normalizer
+
+
+# +
+def normalize_data(x, y):
+    return normalizer(x), y
+
+
+full_training = full_training.map(normalize_data)
+downsampled_training = downsampled_training.map(normalize_data)
+validation = validation.map(normalize_data)
+# -
+
+# This is the model we will be tuning.
+
+
+def build_resnet_18_model(hp):
+    filters_size = [32, 64, 128, 256, 512, 1024]
+
+    def stack_fn(x):
+        x = resnet._stack1(x, hp.Choice(
+            'conv2_filters', filters_size), 3, stride1=1, name='conv2')
+        x = resnet._stack1(x, hp.Choice(
+            'conv2_filters', filters_size), 4, name='conv3')
+        x = resnet._stack1(x, hp.Choice(
+            'conv2_filters', filters_size), 6, name='conv4')
+        return resnet._stack1(x, hp.Choice('conv2_filters', filters_size), 3, name='conv5')
+
+    model = resnet._ResNet(stack_fn,
+                           False,
+                           True,
+                           model_name='random_search_resnet18',
+                           include_top=True,
+                           input_tensor=None,
+                           input_shape=data_shape,
+                           pooling=None,
+                           classes=1,
+                           classifier_activation=None)
+    model.compile(
+        optimizer='adam',
+        loss=tfa.losses.SigmoidFocalCrossEntropy(from_logits=True),
+        metrics=[
+            'binary_accuracy',
+            tfm.RecallScore(from_logits=True),
+            tfm.PrecisionScore(from_logits=True),
+            tfm.F1Score(num_classes=1, from_logits=True, threshold=0.5),
+        ]
+    )
+
+    return model
+
+# Begin our quest for the best parameters.
+
+
+tuner = kt.RandomSearch(
+    build_resnet_18_model,
+    objective=kt.Objective('val_f1_score', direction='max'),
+    max_trials=10
+)
+tuner.search(
+    downsampled_training,
+    epochs = 50,
+    validation_data = validation,
+    class_weight = {1: 1., 0: 1.},
+    shuffle = True,
+    callbacks = [
+        keras.callbacks.EarlyStopping(
+            monitor='val_f1_score',
+            mode='max',
+            verbose=1,
+            patience=10,
+            restore_best_weights=True),
+    ]
+)
