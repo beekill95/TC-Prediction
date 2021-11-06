064704176fa0d08ca679a0a81957c23e153d6980
Quan Nguyen <qmnguyen@iu.edu>
2021-11-06 09:55:40 -0400

    Remove reduntdant code, add plot arbitrary variables

--- tc_formation/plots/observations.py
+++ tc_formation/plots/observations.py
@@ -7,12 +7,16 @@ import xarray as xr
 
 @_d._with_axes
 @_d._with_basemap
-def plot_wind(dataset: xr.Dataset, pressure_level, basemap: Basemap, ax: plt.Axes):
-    latitude = dataset['lat']
-    longitude = dataset['lon']
-
-    lats, longs = np.meshgrid(longitude, latitude)
+def plot_wind(dataset: xr.Dataset, pressure_level: int, basemap: Basemap = None, ax: plt.Axes = None):
+    lats, longs = np.meshgrid(dataset['lat'], dataset['lon'])
     u_wind = dataset['ugrdprs'].sel(lev=pressure_level)
     v_wind = dataset['vgrdprs'].sel(lev=pressure_level)
-
     basemap.barbs(lats, longs, u_wind, v_wind)
+
+
+@_d._with_axes
+@_d._with_basemap
+def plot_variable(dataset: xr.Dataset, variable: str, pressure_level: int, cmap='rainbow', basemap: Basemap = None, ax: plt.Axes = None):
+    lats, longs = np.meshgrid(dataset['lat'], dataset['lon'])
+    data = dataset[variable].sel(lev=pressure_level)
+    basemap.contourf(lats, longs, data, cmap=cmap)
