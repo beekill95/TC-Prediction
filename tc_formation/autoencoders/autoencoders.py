1a86d1de0427a445ff7598e8ef60b90d80853bb2
Quan Nguyen <qmnguyen@iu.edu>
2022-02-10 11:08:47 -0500

    Update autoencoders model

--- tc_formation/autoencoders/autoencoders.py
+++ tc_formation/autoencoders/autoencoders.py
@@ -1,25 +1,65 @@
 import tensorflow.keras as keras
+import tensorflow.keras.backend as backend
 import tensorflow.keras.layers as layers
 
-def AutoEncoders(input_shape, name=None):
-    inputs = layers.Input(shape=input_shape)
+def AutoEncoders(input_shape, input_tensor=None, name=None):
 
+    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
+
+    inputs = (layers.Input(shape=input_shape)
+              if input_tensor is None
+              else input_tensor)
     x = inputs
+
+    # Encoders part.
     x = layers.Conv2D(
-            filters=64,
-            kernel_size=(3, 3),
-            activation='relu',
-            padding='SAME',
-            name=name + '_1_conv')(x)
-    x = layers.MaxPooling2D((2, 2), padding='SAME')(x)
+        64,
+        kernel_size=(3, 3),
+        padding='SAME',
+        name=name + '_1_conv',
+    )(x)
+    x = layers.BatchNormalization(axis=bn_axis, name=name + '_1_bn')(x)
+    x = layers.Activation('relu', name=name + '_1_relu')(x)
+    x = layers.MaxPooling2D(2, strides=2, name=name + '_1_pooling')(x)
 
     x = layers.Conv2D(
-            filters=128,
+        128,
+        kernel_size=(3, 3),
+        padding='SAME',
+        name=name + '_2_conv',
+    )(x)
+    x = layers.BatchNormalization(axis=bn_axis, name=name + '_2_bn')(x)
+    x = layers.Activation('relu', name=name + '_2_relu')(x)
+    x = layers.MaxPooling2D(2, strides=2, name=name + '_2_pooling')(x)
+
+    x = layers.Conv2D(
+        256,
+        kernel_size=(3, 3),
+        padding='SAME',
+        name=name + '_3_conv',
+    )(x)
+    x = layers.BatchNormalization(axis=bn_axis, name=name + '_3_bn')(x)
+    x = layers.Activation('relu', name=name + '_3_relu')(x)
+    x = layers.MaxPooling2D(2, strides=2, name=name + '_3_pooling')(x)
+
+    x = layers.Conv2D(
+        512,
+        kernel_size=(3, 3),
+        padding='SAME',
+        name=name + '_4_conv',
+    )(x)
+    x = layers.BatchNormalization(axis=bn_axis, name=name + '_4_bn')(x)
+    x = layers.Activation('relu', name=name + '_4_relu')(x)
+
+    x = layers.Conv2DTranspose(
+            filters=256,
             kernel_size=(3, 3),
+            strides=2,
             activation='relu',
             padding='SAME',
-            name=name + '_2_conv')(x)
-    x = layers.MaxPooling2D((2, 2), padding='SAME')(x)
+            name=name + '_decoder_1_transposed_conv')(x)
+    x = layers.BatchNormalization(axis=bn_axis, name=name + '_decoder_1_bn')(x)
+    x = layers.Activation('relu', name=name + '_decoder_1_relu')(x)
 
     x = layers.Conv2DTranspose(
             filters=128,
@@ -27,20 +67,27 @@ def AutoEncoders(input_shape, name=None):
             strides=2,
             activation='relu',
             padding='SAME',
-            name=name + '_3_transposed_conv')(x)
+            name=name + '_decoder_2_transposed_conv')(x)
+    x = layers.BatchNormalization(axis=bn_axis, name=name + '_decoder_2_bn')(x)
+    x = layers.Activation('relu', name=name + '_decoder_2_relu')(x)
+
     x = layers.Conv2DTranspose(
             filters=64,
             kernel_size=(3, 3),
             strides=2,
             activation='relu',
             padding='SAME',
-            name=name + '_4_transposed_conv')(x)
+            name=name + '_decoder_3_transposed_conv')(x)
+    x = layers.BatchNormalization(axis=bn_axis, name=name + '_decoder_3_bn')(x)
+    x = layers.Activation('relu', name=name + '_decoder_3_relu')(x)
 
     x = layers.Conv2D(
             filters=input_shape[-1],
             kernel_size=1,
-            name=name + '_output')(x)
+            name=name + '_decoder_output')(x)
 
+    if input_tensor is not None:
+        inputs = keras.utils.get_source_inputs(input_tensor)
     model = keras.Model(inputs, x)
     return model
 
