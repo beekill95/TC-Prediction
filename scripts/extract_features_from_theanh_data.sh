bfbc78445b12b41fb9e48928dba8059b21b4efb5
Quan Nguyen <qmnguyen@iu.edu>
2022-06-14 03:39:54 -0400

    Script for processing all theanh data

---
+++ scripts/extract_features_from_theanh_data.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This is the files that should be processed by the script.
+WRF_FILES=(/N/project/pfec_climo/anhvu/WRFV3/WPAC_RCP45_2???/raw_wrfout_d01_*)
+OUTPUT_DIR="data/theanh_WPAC_RCP45/"
+OUTPUT_PREFIX="theanhRCP45"
+
+# Number of parallel runs.
+N_PROCESSES=4
+
+
+# Now, loop each file and process it with python script.
+for file in "${WRF_FILES[@]}"; do
+    # Make sure we don't use more processes than allowed.
+    ((i=i%N_PROCESSES)); ((i++==0)) && wait
+
+    # Process the file.
+    echo "Processing file $file"
+    ./scripts/extract_features_from_theanh_data.py \
+        "$file" "$OUTPUT_DIR" \
+        --prefix "$OUTPUT_PREFIX" &
+done
+
+# We're done.
+echo '=== DONE!!! ==='
