465b05d0c6ea717135af551fea2767221db0e69d
Quan Nguyen <qmnguyen@iu.edu>
2022-11-11 13:08:02 -0500

    Update

--- scripts/create_labels_v2.py
+++ scripts/create_labels_v2.py
@@ -84,7 +84,7 @@ def load_best_track(
 
     # Convert Longitude.
     # TODO: check this.
-    df['LON'] = df['LON'].apply(lambda l: l if l < 180 else 360 - l)
+    df['LON'] = df['LON'].apply(lambda l: l if l > 0 else 360 + l)
 
     # Group by SID, and only retain the first row.
     genesis_df = df.groupby('SID', sort=False).first()
@@ -96,36 +96,72 @@ def load_best_track(
 
 def create_labels(file_genesis_df, best_track_df) -> pd.DataFrame:
     def has_genesis(row: pd.Series):
-        return True if row['SID'] else False
+        # print(row.notna()['SID'])
+        return row.notna()['SID']
 
     def last_observed(row: pd.Series):
         sid = best_track_df['SID']
         df = best_track_df[sid == row['SID']]
+        # print(df, row)
         return df['Date'].iloc[-1]
+
+    def will_develop_to_tc(row: pd.Series):
+        mask = best_track_df['SID'] == row['SID']
+        df = best_track_df[mask]
+        return 'TS' in df['NATURE'].values
+
+    def develop_to_tc_date(row: pd.Series):
+        if will_develop_to_tc(row):
+            mask = best_track_df['SID'] == row['SID']
+            df = best_track_df[mask]
+            ts_mask = df['NATURE'] == 'TS'
+            return df[ts_mask]['Date'].iloc[0]
+        
+        return None
 
     def other_tc(row: pd.Series):
         original_file_date = row['OriginalDate']
         date_mask = best_track_df['Date'] == original_file_date
-
+        not_current_genesis = best_track_df['SID'] != row['SID']
+        df = best_track_df[date_mask & not_current_genesis]
+        return df
 
     rows = []
-    for _, row in tqdm(file_genesis_df.iterrows()):
-        label = {
-            'Date': row['Date'],
-            'Genesis': has_genesis(row),
-            'TC': has_genesis(row),
-            'TC Id': row['SID'],
-            'Longitude': row['LON'],
-            'Latitude': row['LAT'],
-            'First Observed': row['Date'],
-            'Last Observed': last_observed(row),
-            'First Observed Type': row['Nature'],
-            'Will Develop to TC': 'TODO',
-            'Developing Date': 'TODO',
-            'Is Other TC Happening': 'TODO',
-            'Other TC Locations': 'TODO',
-            'Path': row['Path'],
-        }
+    for _, row in tqdm(file_genesis_df.iterrows(), total=len(file_genesis_df)):
+        is_genesis = has_genesis(row)
+        other_tc_df = other_tc(row)
+        other_tc_locations = zip(
+            other_tc_df['LAT'].tolist(),
+            other_tc_df['LON'].tolist())
+        other_tc_locations = list(other_tc_locations)
+
+        if is_genesis:
+            label = {
+                'Date': row['Date'],
+                'Genesis': is_genesis,
+                'TC': is_genesis,
+                'TC Id': row['SID'],
+                'Longitude': row['LON'],
+                'Latitude': row['LAT'],
+                'First Observed': row['Date'],
+                'Last Observed': last_observed(row),
+                'First Observed Type': row['NATURE'],
+                'Will Develop to TC': will_develop_to_tc(row),
+                'Developing Date': develop_to_tc_date(row),
+                'Is Other TC Happening': len(other_tc_df) > 0,
+                'Other TC Locations': other_tc_locations,
+                'Path': row['Path'],
+            }
+        else:
+            label = {
+                'Date': row['Date'],
+                'Genesis': is_genesis,
+                'TC': is_genesis,
+                'Is Other TC Happening': len(other_tc_df) > 0,
+                'Other TC Locations': other_tc_locations,
+                'Path': row['Path'],
+            }
+
         rows.append(label)
 
     return pd.DataFrame(rows)
@@ -133,8 +169,8 @@ def create_labels(file_genesis_df, best_track_df) -> pd.DataFrame:
 
 def get_domain(path: str) -> tuple[float, float, float, float]:
     ds = xr.load_dataset(path)
-    lat = ds['lat']
-    lon = ds['lon']
+    lat = ds['lat'].values
+    lon = ds['lon'].values
     return lat.min(), lat.max(), lon.min(), lon.max()
 
 
@@ -149,11 +185,14 @@ def main(args=None):
 
     files_df = files_df.sort_values('Date')
 
-    genesis_df, best_track_df = load_best_track(args.best_track)
+    # Get the domain of this dataset by loading the first file.
+    domain = get_domain(files_df['Path'].iloc[0])
+
+    genesis_df, best_track_df = load_best_track(args.best_track, domain)
 
     # Merge these two dataframes.
-    files_genesis_df = files_df.merge(genesis_df, how='left', on='Date')
-    assert len(files_df) == len(files_genesis_df), 'Just to make sure we dont remove rows from files_df'
+    files_genesis_df = pd.merge(files_df, genesis_df, how='left', on='Date')
+    assert len(files_genesis_df) >= len(files_df), 'Just to make sure we dont remove rows from files_df'
 
     labels_df = create_labels(files_genesis_df, best_track_df)
     output_path = os.path.join(
